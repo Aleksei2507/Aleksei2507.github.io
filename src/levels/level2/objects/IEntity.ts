5b47b609c8e3edc4cfc39d568f55a2bb0ca8c8f0
Aleksei Osipov <aosipov@belkacar.ru>
2023-06-27 10:35:24 +0700

    Updates

--- src/levels/level2/objects/IEntity.ts
+++
@@ -1,6 +0,0 @@
-import {PickingInfo} from "@babylonjs/core";
-
-export default interface IEntity {
-    isPicked(pickResult: PickingInfo): boolean;
-    highlightEntity(toggle: boolean): void;
-}
--- src/utils/vectorUtils.ts
+++
@@ -1,5 +0,0 @@
-import {Vector3} from "@babylonjs/core";
-
-export function getDistanceBetweenPoints2D(a: Vector3, b: Vector3) {
-    return Math.sqrt(Math.pow(b.x - a.x, 2) + Math.pow(b.z - a.z, 2))
-}
